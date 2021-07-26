a8963c0c848bf10fafccf704c6723f8b869d8cff
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-07-26 12:39:06 +0300

    00:[Setup project]

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    extends: [
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/react',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    env: {
+        browser: true,
+        jasmine: true,
+        jest: true,
+        node: true,
+    },
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    rules: {
+
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        '@typescript-eslint/no-unused-vars': ['error'],
+    },
+};
