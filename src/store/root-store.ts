1dfc4399b871122c48cc1dc091fcc44a2edaedf2
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-07-28 12:31:58 +0300

    04:add,set up redux-persist

--- src/store/root-store.ts
+++ src/store/root-store.ts
@@ -1,10 +1,23 @@
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import { userReducer } from './user-slice';
 
+const reducers = combineReducers({
+    user: userReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
+    reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
