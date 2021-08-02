6c701209196c61a522517eb926f82fb6204ee884
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-08-02 11:39:37 +0300

    11:add transforme to redux persist

--- src/store/root-store.ts
+++ src/store/root-store.ts
@@ -1,17 +1,20 @@
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
+import createFilter from 'redux-persist-transform-filter';
 import { persistReducer } from 'redux-persist';
 import { userReducer } from './user-slice';
 
 const reducers = combineReducers({
     user: userReducer,
 });
+const saveSubsetFilter = createFilter('user', ['accessToken', 'userId']);
 
 const persistConfig = {
     key: 'root',
     storage,
     whitelist: ['user'],
+    transforms: [saveSubsetFilter],
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
