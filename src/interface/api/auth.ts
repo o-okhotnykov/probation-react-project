3747b4eea5a5800e68ab0a4c086325b3c66ccd09
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-08-03 13:27:06 +0300

    fix bugs

--- src/interface/api/auth.ts
+++ src/interface/api/auth.ts
@@ -1,17 +1,6 @@
 export type LoginResponse = {
     accessToken: string;
-<<<<<<< HEAD
     user: IUserData;
-=======
-    error: { message: string };
-    user: {
-        birthDate: string;
-        email: string;
-        id: number;
-        name: string;
-        surname: string;
-    };
->>>>>>> df8b6e103352dcef61aa4d287508bc64b8829813
 };
 
 export type RegisterResponse = {
