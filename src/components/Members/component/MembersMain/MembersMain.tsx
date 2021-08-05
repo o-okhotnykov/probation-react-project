9d55bf03724108c7dad9b027c2130c7aa8c5b7c7
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-08-05 18:31:35 +0300

    members markup

--- src/components/Members/component/MembersMain/MembersMain.tsx
+++ src/components/Members/component/MembersMain/MembersMain.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Button, Grid, Typography } from '@material-ui/core';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import { useStyles } from './styles';
 
 export const MembersMain: React.FC = () => {
-    return <div>Members</div>;
+    const classes = useStyles();
+    return (
+        <div>
+            <Grid container spacing={1} className={classes.container}>
+                <Grid item xs={5} className={classes.addProject}>
+                    <Typography variant="h5" className={classes.header}>
+                        Add Project
+                    </Typography>
+                    <Typography variant="body2" className={classes.subTitle}>
+                        Create a new project on ProManage. Collaborate your work. Directory to your
+                        local projects
+                    </Typography>
+                    <Typography>
+                        <Button className={classes.btn}>
+                            <AddCircleIcon style={{ fontSize: 40 }} className="plus-icon" />
+                            CREATE New Project
+                        </Button>
+                    </Typography>
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
