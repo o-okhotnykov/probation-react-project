eb34694a442e84e589aa32391db27497edb35097
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-08-05 16:46:26 +0300

    f

--- src/components/Navigation/component/Navbar/Navbar.tsx
+++ src/components/Navigation/component/Navbar/Navbar.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import { ROUTE_PATH } from 'constants/index';
-import { useStyles } from './styles';
 
 export const Navbar: React.FC = () => {
     return (
         <Typography>
-            <Link to={ROUTE_PATH.dashboard}>Dashboard</Link>
-            <Link to={ROUTE_PATH.dashboard}>All Projects</Link>
-            <Link to={ROUTE_PATH.members}>Members</Link>
-            <Link to={ROUTE_PATH.dashboard}>Stats</Link>
-            <Link to={ROUTE_PATH.dashboard}>Help</Link>
+            <NavLink to={ROUTE_PATH.dashboard}>Dashboard</NavLink>
+            <NavLink to={ROUTE_PATH.dashboard}>All Projects</NavLink>
+            <NavLink to={ROUTE_PATH.members}>Members</NavLink>
+            <NavLink to={ROUTE_PATH.dashboard}>Stats</NavLink>
+            <NavLink to={ROUTE_PATH.dashboard}>Help</NavLink>
         </Typography>
     );
 };
