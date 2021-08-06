7f09a8c456aadf154ea4f6b4816340952fbb9651
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-08-06 16:45:41 +0300

    add sort

--- src/components/Table/component/Table.tsx
+++ src/components/Table/component/Table.tsx
@@ -1,16 +1,22 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable react/jsx-key */
 import React from 'react';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
-import { useTable } from 'react-table';
+import { useSortBy, useTable } from 'react-table';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import { useStyles } from './styles';
 
 export const TableComponent = ({ columns, data }: any) => {
     const classes = useStyles();
 
-    const { getTableProps, headerGroups, rows, prepareRow } = useTable({
-        columns,
-        data,
-    });
+    const { getTableProps, headerGroups, rows, prepareRow } = useTable(
+        {
+            columns,
+            data,
+        },
+        useSortBy,
+    );
 
     return (
         <Table {...getTableProps()} className={classes.table}>
@@ -18,8 +24,21 @@ export const TableComponent = ({ columns, data }: any) => {
                 {headerGroups.map((headerGroup) => (
                     <TableRow {...headerGroup.getHeaderGroupProps()}>
                         {headerGroup.headers.map((column) => (
-                            <TableCell {...column.getHeaderProps()} className={classes.tableHead}>
+                            <TableCell
+                                {...column.getHeaderProps(column.getSortByToggleProps())}
+                                className={classes.tableHead}
+                            >
                                 {column.render('Header')}
+
+                                {column.isSorted ? (
+                                    column.isSortedDesc ? (
+                                        <ArrowDropDownIcon />
+                                    ) : (
+                                        <ArrowDropUpIcon />
+                                    )
+                                ) : (
+                                    ''
+                                )}
                             </TableCell>
                         ))}
                     </TableRow>
