cf449ae2b4ee4e2eab6a6ad5c8689e0c30439439
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-08-09 15:26:28 +0300

    add type to middleware

--- src/store/middleware.ts
+++ src/store/middleware.ts
@@ -1,6 +1,7 @@
+import { Middleware } from 'redux';
 import { startLoading, finishLoading } from './loading-slice';
 
-export const loadingHandler = (store: any) => (next: any) => (action: any) => {
+export const loadingHandler: Middleware = (store) => (next) => (action) => {
     if (action && action.meta && action.meta.requestStatus === 'pending') {
         store.dispatch(startLoading());
     }
