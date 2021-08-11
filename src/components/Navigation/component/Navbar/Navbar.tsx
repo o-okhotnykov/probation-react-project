3dff019ab904bf18078835d6606f0943f67f7047
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-08-11 16:06:26 +0300

    change navlink to tabs

--- src/components/Navigation/component/Navbar/Navbar.tsx
+++ src/components/Navigation/component/Navbar/Navbar.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useHistory, withRouter } from 'react-router-dom';
 import { ROUTE_PATH } from 'constants/index';
+import { Tab, Tabs } from '@material-ui/core';
+import { Dashboard } from '@material-ui/icons';
+import { MembersMain } from 'components/Members';
+import { ErrorComponent } from 'components/ErrorComponent';
+import { useStyles } from './styles';
 
-export const Navbar: React.FC = () => {
+const NavbarComponent: React.FC = () => {
+    const history = useHistory();
+    const classes = useStyles();
+
     return (
-        <nav className="nav-list">
-            <NavLink to={ROUTE_PATH.dashboard} className="nav-item" activeClassName="nav-selected">
-                Dashboard
-            </NavLink>
-            <NavLink to={ROUTE_PATH.projects} className="nav-item" activeClassName="nav-selected">
-                All project
-            </NavLink>
-            <NavLink to={ROUTE_PATH.members} className="nav-item" activeClassName="nav-selected">
-                Members
-            </NavLink>
-            <NavLink to={ROUTE_PATH.stats} className="nav-item" activeClassName="nav-selected">
-                Stats
-            </NavLink>
-            <NavLink to={ROUTE_PATH.help} className="nav-item" activeClassName="nav-selected">
-                Help
-            </NavLink>
-        </nav>
+        <Tabs value={history.location.pathname} onChange={(event, value) => history.push(value)}>
+            <Tab className={classes.tab} label="Dashboard" value={ROUTE_PATH.dashboard}>
+                <Dashboard />
+            </Tab>
+            <Tab className={classes.tab} label="All projects" value={ROUTE_PATH.projects}>
+                <ErrorComponent />
+            </Tab>
+            <Tab className={classes.tab} label="Members" value={ROUTE_PATH.members}>
+                <MembersMain />
+            </Tab>
+            <Tab className={classes.tab} label="Stats" value={ROUTE_PATH.stats}>
+                <ErrorComponent />
+            </Tab>
+            <Tab className={classes.tab} label="Help" value={ROUTE_PATH.help}>
+                <ErrorComponent />
+            </Tab>
+        </Tabs>
     );
 };
+
+export const Navbar = withRouter(NavbarComponent);
