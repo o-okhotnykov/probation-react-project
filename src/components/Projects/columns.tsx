577f70ebd46e578b3735d9790820b00a22928f80
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-09-01 12:08:42 +0300

    change table in project page

--- src/components/Projects/columns.tsx
+++ src/components/Projects/columns.tsx
@@ -1,40 +1,18 @@
 import { ReactElement } from 'react';
-import { CardMedia, Paper, Typography } from '@material-ui/core';
+import { Box, CardMedia, Paper, Typography } from '@material-ui/core';
 import { Column } from 'react-table';
 import { ProjectState } from 'types/api/project';
 import { useStyles } from './styles';
 
-// const progressStyle: {
-//     [key in ProjectState]: CSSProperties;
-// } = {
-//     open: {
-//         color: theme.palette.info.main,
-//         backgroundColor: theme.palette.info.light,
-//         textTransform: 'uppercase',
-//         ...memberStatus,
-//     },
-//     done: {
-//         color: theme.palette.success.main,
-//         backgroundColor: theme.palette.success.light,
-//         textTransform: 'uppercase',
-//         ...memberStatus,
-//     },
-//     progress: {
-//         color: theme.palette.warning.main,
-//         backgroundColor: theme.palette.warning.light,
-//         textTransform: 'uppercase',
-//         ...memberStatus,
-//     },
-// };
-
 export const columns: Column[] = [
     {
-        Header: function Img(): ReactElement {
-            return <Typography variant="h3">Image</Typography>;
-        },
         accessor: 'img',
         Cell: function Name({ value }: { value: string }): ReactElement {
-            return <CardMedia style={{ width: '5vh', height: '5vh' }} image={value} />;
+            return (
+                <Box display="flex" justifyContent="center" alignItems="ce">
+                    <CardMedia style={{ width: '5vh', height: '5vh' }} image={value} />
+                </Box>
+            );
         },
     },
     {
@@ -47,8 +25,17 @@ export const columns: Column[] = [
         },
     },
     {
+        Header: function Views(): ReactElement {
+            return <Typography variant="h3">Views</Typography>;
+        },
+        accessor: 'views',
+        Cell: function Views({ value }: { value: string }): ReactElement {
+            return <Typography variant="body1">{value}</Typography>;
+        },
+    },
+    {
         Header: function dateCreate(): ReactElement {
-            return <Typography variant="h3">Date Create</Typography>;
+            return <Typography variant="h3">Created</Typography>;
         },
         accessor: 'dateCreate',
         Cell: function dateCreate({ value }: { value: string }): ReactElement {
@@ -57,7 +44,7 @@ export const columns: Column[] = [
     },
     {
         Header: function dateDue(): ReactElement {
-            return <Typography variant="h3">Date Due</Typography>;
+            return <Typography variant="h3">Done</Typography>;
         },
         accessor: 'dateDue',
         Cell: function dateDue({ value }: { value: string }): ReactElement {
