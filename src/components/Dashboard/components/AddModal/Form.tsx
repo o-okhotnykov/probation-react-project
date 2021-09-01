ab1680886a346fe033e7273236d3867ff12ffe85
Oleksandr Okhotnykov <okhotnykov.oleksandr@nixsolutions.com>
2021-09-01 17:56:27 +0300

    add day picker dashboard

--- src/components/Dashboard/components/AddModal/Form.tsx
+++ src/components/Dashboard/components/AddModal/Form.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Grid, TextField } from '@material-ui/core';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { format } from 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
 import { useFormik } from 'formik';
-import { getUserAsync, userDataSelector } from 'store/user-slice';
+import { userDataSelector } from 'store/user-slice';
 import { useDispatch, useSelector } from 'react-redux';
 import { fileToBase64 } from 'helper/base64';
 import defaultProject from 'assets/default-project.png';
 import { Loading } from 'components/Loading';
 import { Project, ProjectState } from 'types/api/project';
 import { addProjectAsync } from 'store/project-slice';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 import { editFormValidator } from './validation';
 import { useStyles } from './styles';
 
@@ -23,10 +26,6 @@ export const Form: React.FC<FormProps> = ({ submit, handleCloseModal }) => {
     const currentDay = format(new Date(), 'yyyy-MM-dd');
     const currentUser = useSelector(userDataSelector);
 
-    useEffect(() => {
-        dispatch(getUserAsync());
-    }, [dispatch]);
-
     const [value, setValue] = useState<Project>({
         id: 0,
         title: '',
@@ -34,7 +33,8 @@ export const Form: React.FC<FormProps> = ({ submit, handleCloseModal }) => {
         dateCreate: '',
         dateDue: '',
         stats: ProjectState.open,
-        img: '',
+        img: defaultProject,
+        views: 0,
     });
 
     useEffect(() => {
@@ -47,6 +47,7 @@ export const Form: React.FC<FormProps> = ({ submit, handleCloseModal }) => {
                 dateDue: '',
                 stats: ProjectState.open,
                 img: defaultProject,
+                views: 0,
             });
         }
     }, [currentUser, currentDay]);
@@ -62,6 +63,16 @@ export const Form: React.FC<FormProps> = ({ submit, handleCloseModal }) => {
     ) => {
         const data = await fileToBase64(event.currentTarget.files![0]);
         setFieldValue('img', data);
+    };
+
+    const handleChangeDate = (
+        event: MaterialUiPickersDate,
+        setFieldValue: (field: string, value: unknown) => void,
+    ) => {
+        if (event) {
+            const date = format(event, 'yyyy-MM-dd');
+            setFieldValue('dateDue', date);
+        }
     };
 
     const formik = useFormik({
@@ -84,65 +95,73 @@ export const Form: React.FC<FormProps> = ({ submit, handleCloseModal }) => {
     } = formik;
 
     return (
-        <Loading apiCall={addProjectAsync}>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <form onSubmit={handleSubmit}>
-                <Grid container>
-                    <Grid item xs={6}>
-                        <TextField
-                            id="title"
-                            label="Title"
-                            value={values.title}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            helperText={touched.title ? errors.title : ''}
-                            error={touched.title && Boolean(errors.title)}
-                            margin="dense"
-                            variant="outlined"
-                            fullWidth
-                        />
-
-                        <TextField
-                            id="dateDue"
-                            type="date"
-                            InputProps={{ inputProps: { min: currentDay } }}
-                            value={values.dateDue}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            helperText={touched.dateDue ? errors.dateDue : ''}
-                            error={touched.dateDue && Boolean(errors.dateDue)}
-                            margin="dense"
-                            variant="outlined"
-                            fullWidth
-                        />
-                    </Grid>
-                    <Grid item xs={6} className={classes.formPart}>
-                        <img src={values.img} alt="user-img" className={classes.userImg} />
-                        <Button variant="contained" component="label" color="primary">
-                            Upload File
-                            <input
-                                style={{ display: 'none' }}
-                                id="file"
-                                name="file"
-                                type="file"
-                                onChange={(event) => handleUpload(event, setFieldValue)}
+                <Loading apiCall={addProjectAsync}>
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="title"
+                                label="Title"
+                                value={values.title}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                helperText={touched.title ? errors.title : ''}
+                                error={touched.title && Boolean(errors.title)}
+                                margin="dense"
+                                placeholder="Title"
+                                variant="outlined"
+                                fullWidth
                             />
-                        </Button>
+
+                            <KeyboardDatePicker
+                                disableToolbar
+                                margin="normal"
+                                format="yyyy-MM-dd"
+                                inputVariant="outlined"
+                                placeholder="yyyy-MM-dd"
+                                id="dateDue"
+                                disablePast
+                                value={values.dateDue}
+                                onChange={(event) => handleChangeDate(event, setFieldValue)}
+                                onBlur={handleBlur}
+                                helperText={touched.dateDue ? errors.dateDue : ''}
+                                error={touched.dateDue && Boolean(errors.dateDue)}
+                            />
+                        </Grid>
+                        <Grid item xs={6} className={classes.formPart}>
+                            <img src={values.img} alt="user-img" className={classes.userImg} />
+                            <Button variant="contained" component="label" color="primary">
+                                Upload File
+                                <input
+                                    style={{ display: 'none' }}
+                                    id="file"
+                                    name="file"
+                                    type="file"
+                                    onChange={(event) => handleUpload(event, setFieldValue)}
+                                />
+                            </Button>
+                        </Grid>
+                        <Grid item className={classes.action} xs={12}>
+                            <Button
+                                color="secondary"
+                                variant="contained"
+                                onClick={handleCloseModal}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                color="primary"
+                                type="submit"
+                                variant="contained"
+                                disabled={!isValid || !dirty}
+                            >
+                                Confirm
+                            </Button>
+                        </Grid>
                     </Grid>
-                    <Grid item className={classes.action} xs={12}>
-                        <Button color="secondary" variant="contained" onClick={handleCloseModal}>
-                            Cancel
-                        </Button>
-                        <Button
-                            color="primary"
-                            type="submit"
-                            variant="contained"
-                            disabled={!isValid || !dirty}
-                        >
-                            Confirm
-                        </Button>
-                    </Grid>
-                </Grid>
+                </Loading>
             </form>
-        </Loading>
+        </MuiPickersUtilsProvider>
     );
 };
