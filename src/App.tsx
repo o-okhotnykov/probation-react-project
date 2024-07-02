5072f16ad27b12e0eed1e28b492bed4b2f6ba059
o-okhotnykov <sashaohotni26@gmail.com>
2024-07-02 15:50:38 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter as Router } from 'react-router-dom';
 import DateFnsUtils from '@date-io/date-fns';
-import { Routes } from 'routes/Routes';
-import 'react-toastify/dist/ReactToastify.css';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { theme } from 'components/theme';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { Routes } from 'routes/Routes';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App: React.FC = () => {
     return (
